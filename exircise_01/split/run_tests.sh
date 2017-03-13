4fb333e586cc28b9ffb843e5c2b6360ca4be7c48
Georgy Ivanov <ivanov.georg@gmail.com>
2017-03-13 03:19:02 +0300

    add test runner for split tool

---
+++ exircise_01/split/run_tests.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROG=$1
+[ -z $PROG ] && PROG="${DIR}"/split.py
+
+set -euo pipefail 
+
+ALL_COUNT=0
+OK_COUNT=0
+for data in $(ls ${DIR}/*.dat); do
+    ans="${data%.*}".ans
+    out="${data%.*}".out
+    args="${data%.*}".args
+    err="${data%.*}".err
+     
+    ret=0
+    cat "${data}" | ${PROG} $(head -n1 ${args}) 1>${out} 2>${err} || ret=$?
+    
+    if [ $ret -ne 0 ]; then
+        echo "ERR:"
+        head ${err} 
+        echo "OUT:"
+        head ${out}
+    elif cmp ${out} ${ans}; then
+        OK_COUNT=$(( $OK_COUNT + 1 ))
+    fi
+    ALL_COUNT=$(( $ALL_COUNT + 1 )) 
+done
+
+echo $OK_COUNT / $ALL_COUNT tests OK
+if [ $OK_COUNT -eq $ALL_COUNT ]; then
+    exit 0
+fi
+exit 1
